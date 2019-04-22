3554c3aa34d6cfd11f87c71eaad2ea0b2ceaf841
Lucas Batista <lucaophp@hotmail.com>
2019-04-21 21:26:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <head>
-	<title>OpenLayers Simplest Example</title>
+	<title>Coletor de Incidentes</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
